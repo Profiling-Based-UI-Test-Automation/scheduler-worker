ae2f5d55faeef388431ee1d9d20fdf0c26e0b775
heeseon <domich.hwang@gmail.com>
2018-11-13 23:42:00 +0900

    앱 처음 실행시, rabbitmq에 등록된 디바이스들에 대해 큐 만들고, 큐 워커 서비스 만듦

--- src/main/java/scheduler/db/SchedulerInitializer.java
+++ src/main/java/scheduler/db/SchedulerInitializer.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.core.RabbitManagementTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SchedulerInitializer implements InitializingBean {
@@ -21,18 +22,23 @@ public class SchedulerInitializer implements InitializingBean {
     private DeviceAvailabilityChangesListener deviceAvailabilityChangesListener;
 
     private static final RethinkDB r = RethinkDB.r;
+    
+    private static List<String> listenerlist = new ArrayList<String>();
 
     @Override
     public void afterPropertiesSet() throws Exception {
     	
+    	// 현재 등록된 모든 큐를 가져온다. 
     	RabbitManagementTemplate rmt = new RabbitManagementTemplate("http://localhost:15672/api/", "guest", "guest");
 
     	List<Queue> list = rmt.getQueues();
     	
     	for(int i = 0 ; i < list.size() ; ++ i) {
     		String serial = list.get(i).getName();
-    		if( serial != null )
+    		if( serial != null ) {
     			registerBootedDevices(serial);
+    			listenerlist.add(serial);
+    		}
     	}
     	
     	System.out.println("====================== list.length = " + list.size());
@@ -41,6 +47,8 @@ public class SchedulerInitializer implements InitializingBean {
         
     }
 
+    //파라미터로 전달된 큐 이름이 stf device에 목록 상에 있는지 확인하고, 있는 경우 서비스를 실행시킨다. 
+    //서비스는 해당 stf의 device에 대한 chagefeed를 걸어, 디바이스가 유휴해지거나 예약되는 것에 대한 알림을 받는다. 
     private void registerBootedDevices(String serial) {
     	Connection<ConnectionInstance> connection = connectionFactory.createConnection();
         List<String> dbList = r.dbList().run(connection);
